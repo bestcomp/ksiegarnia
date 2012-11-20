2787412b713e39f0fafa2c207af034a6204f1c07
bestcomp <bestcomp@o2.pl>
2012-11-20 11:49:30 +0100

    projekt rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ksiegarnia::Application.config.secret_token = 'b8bbb1837f7e70760c0b3cdbbe11918d8e662c1a1a64dd85c467994fd2c415a689a5269eaeafbb0ff160b11b3255f67d605d33a65e3910401af436d731a1b8c1'
