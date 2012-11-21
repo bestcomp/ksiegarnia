059dd7326cc1886cd63eac803b0fb22d3befa4e2
bestcomp <bestcomp@o2.pl>
2012-11-21 15:25:51 +0100

    czesctrzecia

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,15 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-		validates :title, :description, :image_url, :presence => true
-		validates :title, :uniqueness => true	
-		
-		#validates :price, numericality: { greater_than: 0.01}
-		validates :image_url, 
-			allow_blank: true, 
-			format: {
-				with: %r{\.(gif|jpg|png)\Z}i,
-				message: 'must be a URL for GIF, JPG or PNG image.'
-			}
+	
+	validates :title, :description, :image_url, :presence => true
+	validates :title, :uniqueness => true			
+	validates :price, numericality: { greater_than: 0.01}
+	validates :image_url, 
+		allow_blank: true, 
+		format: {
+			with: %r{\.(gif|jpg|png)\Z}i,
+			message: 'must be a URL for GIF, JPG or PNG image.'
+		}
+
+    has_many :line_items, dependent: :destroy
 end
