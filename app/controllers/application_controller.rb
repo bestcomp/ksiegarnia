9f8f99b84b6760a8149e6865f6163b5bac4187c4
bestcomp <bestcomp@o2.pl>
2012-11-23 09:24:54 +0100

    dzien piaty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@
 class ApplicationController < ActionController::Base
 
     protect_from_forgery
-
+before_filter :ustawkoszyk
 	helper_method :current_cart
 
     private
-
+	def ustawkoszyk
+	@cart = current_cart
+	end
       def current_cart
         Cart.find(session[:cart_id])
       rescue ActiveRecord::RecordNotFound
--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,6 @@ class Product < ActiveRecord::Base
 		}
 
     has_many :line_items, dependent: :destroy
+
+  	has_many :orders, through: :line_items 
 end
