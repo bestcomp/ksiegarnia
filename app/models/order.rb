a6019ed926f0fe81a3c774d83628ac07fcdcf75d
bestcomp <bestcomp@o2.pl>
2012-11-23 09:32:46 +0100

    add file

---
+++ app/models/order.rb
@@ -0,0 +1,20 @@
+#coding: UTF-8
+class Order < ActiveRecord::Base
+
+  
+	PAYMENT_TYPES = [ "Przelew", "Odbiór własny" ]
+  attr_accessible :address, :email, :name, :pay_type
+
+	validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+
+	has_many :line_items, dependent: :destroy
+	has_many :products, through: :line_items
+
+			def add_line_items_from_cart(cart)
+    	 cart.line_items.each do |item|
+        item.cart_id = nil
+       	line_items << item
+     	 end
+    	end
+end
---
+++ test/functional/order_notifier_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class OrderNotifierTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderNotifier.received
+    assert_equal "Received", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderNotifier.shipped
+    assert_equal "Shipped", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "create" do
+    mail = OrderNotifier.create
+    assert_equal "Create", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "app/mailers/order_notifier.rb" do
+    mail = OrderNotifier.app/mailers/order_notifier.rb
+    assert_equal "App/mailers/order notifier.rb", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
